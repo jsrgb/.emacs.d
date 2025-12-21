dab8f98416a382b737e99d44cf384c221bf6c5f0
Jacob Santos <148006242+jsrgb@users.noreply.github.com>
2025-12-20 19:28:10 -0800

    Add files via upload

--- init.el
+++ init.el
@@ -1,9 +1,9 @@
-;;; .emacs for terminal
+;;; .emacs
 ;;; Commentary
 ;;; Code:
 
-(when (version<  emacs-version "30.0")
-  (error "Config not tested on v%s. Please use v30.0 or higher." emacs-version))
+(when (version<  emacs-version "30.1")
+  (error "Config not tested on v%s. Please use v30.1 or higher." emacs-version))
 
 (setq default-frame-alist
       '((fullscreen . maximized)
@@ -21,9 +21,11 @@
         ))
 (package-initialize)
 
-(exec-path-from-shell-initialize)
-(exec-path-from-shell-copy-envs '("PATH"))
-(setenv "LIBRARY_PATH" "/opt/homebrew/lib")
+(when (memq window-system '(mac ns x))
+  (exec-path-from-shell-initialize))
+;; (exec-path-from-shell-initialize)
+;; (exec-path-from-shell-copy-envs '("PATH"))
+;; (setenv "LIBRARY_PATH" "/opt/homebrew/lib")
 
 (unless (bound-and-true-p package--initialized)
   (setq package-enable-at-startup nil) ;; Prevent double-loading packages
@@ -38,8 +40,6 @@
   (when (not (package-installed-p package))
     (package-install package)))
 
-(load-theme 'zenburn t)
-
 ;;;
 ;;; Defaults
 ;;;
@@ -52,6 +52,7 @@
 (fset 'yes-or-no-p 'y-or-n-p)
 (delete-selection-mode t)
 (fringe-mode '(10 . 0))
+(global-hl-line-mode)
 (setq inhibit-startup-screen t
       initial-scratch-message nil
       ring-bell-function 'ignore
@@ -65,42 +66,60 @@
               indent-tabs-mode nil
               tab-width 4)
 
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 6))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-expression (read (current-buffer)))))
+  (load bootstrap-file nil 'nomessage))
+
+;; Install and configure use-package
+(straight-use-package 'use-package)
+(use-package straight
+  :custom (straight-use-package-by-default t))
+
+;;
+;; Local packages
+;;
+(add-to-list 'load-path "~/.emacs.d/custom/")
+
+(pm/use 'base16-theme)
+(pm/use 'doom-themes)
+(load-theme 'doom-opera t)
+
 (setq native-comp-async-report-warnings-errors 'silent)
 
 (setopt display-fill-column-indicator-column 80)
 (add-hook 'prog-mode-hook 'display-fill-column-indicator-mode)
 
-(setq display-line-numbers-type 'absolute)
-(global-display-line-numbers-mode +1)
+;;;
+;;; OS Specific
+;;;
+(defvar cfg-loc "")
+(set-frame-font "Terminus (TTF) 16" nil t)
 
-(pm/use 'rg)
-(rg-enable-default-bindings)
+(when (eq system-type 'windows-nt)
+  (set-face-attribute 'default nil :font "Consolas-12")
+  (setq cfg-loc "C:/Users/ja/AppData/Roaming/.emacs.d/init.el")
+  (setq default-directory "C:/Workspace/"))
 
-(global-set-key (kbd "C-c r") (lambda () (interactive) (recompile)))
-(global-set-key (kbd "C-c `") 'next-error)
+(when (eq system-type 'darwin)
+  (set-face-attribute 'default nil :font "Lilex-16")
+  (setq mac-command-modifier 'meta)
+  (setq cfg-loc "~/.emacs.d/init.el"))
 
-(use-package vertico
-  :ensure t
-  :init (vertico-mode 1))
+(when (eq system-type 'gnu/linux)
+  (setq cfg-loc "~/.emacs.d/init.el")
+  (set-face-attribute 'default nil :font "Hack-12"))
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("806dd05c68b646416d686fc45d1ed7e6a173511e2548cd62150473fe5149f66c"
-     default))
- '(package-selected-packages
-   '(consult exec-path-from-shell glsl-mode json-mode lua-mode magit
-             move-text multiple-cursors rg rust-mode typescript-mode
-             vertico wgsl-mode yaml-mode zenburn-theme zig-mode)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(global-set-key (kbd "C-i") 'xref-go-forward)
+(global-set-key (kbd "C-o") 'xref-go-back)
 
 ;;;
 ;;; Window and Layout Handling
@@ -112,7 +131,7 @@
 (global-unset-key (kbd "M-o"))
 (global-unset-key (kbd "M-`"))
 (global-set-key (kbd "M-o") (lambda () (interactive) (other-window 1)))
-(global-set-key (kbd "M-O") (lambda () (interactive) (other-window -1)))
+;;(global-set-key (kbd "M-O") (lambda () (interactive) (other-window -1)))
 
 
 (defun save-layout-delete-other-windows ()
@@ -140,7 +159,6 @@
 (global-set-key (kbd "C->") 'mc/mark-next-like-this)
 (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
 (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
-
 
 (pm/use 'move-text)
 (move-text-default-bindings)
@@ -179,11 +197,72 @@
 (pm/use 'typescript-mode)
 
 ;;;
+;;; Projects
+
+;;;
+;;; Compilation
+;;;
+(global-set-key (kbd "C-c r") (lambda () (interactive) (recompile)))
+(global-set-key (kbd "C-c `") 'next-error)
+
+;;;
+;;; Misc
+;;;
+(defun cfg ()
+  "Open the `.emacs` or `init.el`."
+  (interactive)
+  (find-file cfg-loc))
+
+;;;
 ;;; UI
 ;;;
 
 (pm/use 'which-key)
 (which-key-mode)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("1cfbec19edafb831c7729be2f6454ec019c21b9a54b39b3bb5ec276a6b21d484"
+     "df3d4f912f543ea8455e446f23c5d4d7ce76d01776a0a7239b444d73a2fc5184"
+     "712dda0818312c175a60d94ba676b404fc815f8c7e6c080c9b4061596c60a1db"
+     "dea106ab256a8017a325f51f01b1131915989fa25db48eb831ffb18dac8ecd39"
+     "fbf73690320aa26f8daffdd1210ef234ed1b0c59f3d001f342b9c0bbf49f531c"
+     "2e7dc2838b7941ab9cabaa3b6793286e5134f583c04bde2fba2f4e20f2617cf7"
+     "f5f070872db3e4d8b82dbb2f3b1c60beca86fc93327a38ebddd22070458a14bc"
+     "a12f585b1ff1b35f4000eab71f9f20a784b94f797296de13d467f9b3021b9a8b"
+     "e338de851db9cb260207b8b7246761585e79a489c7750110e01c10e216af495f"
+     "76c92281dc2f878bce2ab4b4466f76afda48c1ed95dfb3a97da31df39b21491d"
+     "882d6a5981fd85d9f987d31623e25e69b8d5635a53ab442f1a51e7c252790320"
+     "308ead333d5dfc7d64258a9d3d46f9e479e4b62bdf33a8ea8700423842cae32e"
+     "6ed98f47da7556a8ce6280346e5d8e1e25bede71dc5186aa2654b93bec42d2a6"
+     "1f82b7df8a4ce0e2ba3b0217d2c552b2fe5b5dd1244481cb65bef733f9818c63"
+     "d8b8c09a745470f6c088dce5df19ade98894f4ced69ce32d53aded94d512826d"
+     "623e9fe0532cc3a0bb31929b991a16f72fad7ad9148ba2dc81e395cd70afc744"
+     "e8915dac580af7a4a57bd38a0638a5a7714b47714529b1a66bd19e7aa155818d"
+     default))
+ '(dape-key-prefix [24 1] t)
+ '(eglot-ignored-server-capabilities
+   '(:hoverProvider :signatureHelpProvider :codeActionProvider
+                    :codeLensProvider :documentLinkProvider
+                    :foldingRangeProvider :inlayHintProvider))
+ '(flycheck-check-syntax-automatically '(save idle-change mode-enabled))
+ '(package-selected-packages
+   '(amx base16-theme bufferlo dap-mode expand-region go-mode gptel
+         icomplete-vertical json-mode llm magit markdown-mode
+         move-text multiple-cursors request twittering-mode
+         vs-light-theme which-key whiteboard yaml-mode zig-mode))
+ '(persp-modestring-short t)
+ '(persp-show-modestring t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
 
 (setq gc-cons-threshold (* 100 1024 1024))
 
@@ -200,7 +279,18 @@
                '((typescript-mode tsx-ts-mode typescript-ts-mode)
                  . ("vtsls" "--stdio"))
                '((rust-ts-mode rust-mode) .
-               ("rust-analyzer" :initializationOptions (:check (:command "check"))))))
+                 ("rust-analyzer" :initializationOptions (:check (:command "check"))))))
+
+(cl-defmethod eglot-handle-notification :after
+  (_server (_method (eql textDocument/publishDiagnostics)) &key uri
+           &allow-other-keys)
+  (when-let ((buffer (find-buffer-visiting (eglot-uri-to-path uri))))
+    (with-current-buffer buffer
+      (if (and (eq nil flymake-no-changes-timeout)
+               (not (buffer-modified-p)))
+          (flymake-start t)))))
+
+(global-set-key (kbd "C-.") #'completion-at-point)
 
 ;;
 ;; Flymake
@@ -208,6 +298,87 @@
 (global-set-key (kbd "C-c e b") 'flymake-show-buffer-diagnostics)
 (global-set-key (kbd "C-c e p") 'flymake-show-project-diagnostics)
 
+;;
+;; Org
+;;
+(load-file "~/.emacs.d/om.el")
+(pm/use 'bufferlo)
+
+;;
+;; Debug
+;;
+(pm/use 'dape)
+(setq dape-buffer-window-arrangement 'right)
+(setq dape-key-prefix (kbd "C-x C-a"))
+
+;;
+;; Kill-ring
+;;
+(pm/use 'browse-kill-ring)
+(global-unset-key (kbd " M-y"))
+(global-set-key (kbd " M-y") 'browse-kill-ring)
+
+(add-to-list 'exec-path "/Users/j/.local/bin")
+
+;;
+;; LLMs
+;;
+(global-unset-key (kbd "<C-wheel-up>"))
+(global-unset-key (kbd "<C-wheel-down>"))
+
+(setq straight-repository-branch "develop")
+
+(setq display-line-numbers-type 'relative)
+(global-display-line-numbers-mode +1)
+
+(load-file "~/.emacs.d/ellm.el")
+(setq warning-minimum-level :error)
+
+(pm/use 'rg)
+(rg-enable-default-bindings)
+
+;; Basic org-roam setup - just the essentials
+(use-package org-roam
+  :ensure t
+  :custom
+  (org-roam-directory "~/Workspace/notes")
+  :bind (("C-c n f" . org-roam-node-find)
+         ("C-c n i" . org-roam-node-insert))
+  :config
+  (org-roam-setup))
+
+(use-package org-roam-ui
+  :ensure t
+  :after org-roam
+  :config
+  (setq org-roam-ui-sync-theme t
+        org-roam-ui-follow t
+        org-roam-ui-update-on-save t))
+
+(load "~/.emacs.d/odin-mode.el")
+
+(use-package vertico
+  :ensure t
+  :init (vertico-mode 1))
+
+(use-package vertico-posframe
+  :ensure t
+  :after vertico
+  :config
+  (setq vertico-posframe-border-width 2)
+  :init (vertico-posframe-mode 1))
+
+(setq vertico-multiform-commands
+      '((consult-ripgrep (:not posframe))
+        (consult-line (:not posframe))
+        (t posframe)))
+
+(use-package orderless
+  :ensure t
+  :init
+  (setq completion-styles '(orderless basic)
+        completion-category-defaults nil
+        completion-category-overrides '((file (styles basic)))))
 
 (use-package consult
   :ensure t
@@ -217,4 +388,36 @@
   (global-set-key (kbd "M-O") 'consult-imenu)
   (global-set-key (kbd "M-F") 'consult-ripgrep)
   (global-set-key (kbd "M-s") 'consult-line)
+  (global-set-key (kbd "M-g M-g") 'consult-goto-line)
   )
+
+(use-package apheleia
+  :ensure t
+  :config
+  (add-hook 'prog-mode-hook #'apheleia-mode))
+
+(use-package corfu
+  :ensure t
+  :init
+  (global-corfu-mode 1)
+  :custom
+  (corfu-auto nil))
+
+(add-hook 'markdown-mode-hook (lambda () (corfu-mode -1)))
+(add-hook 'text-mode-hook (lambda () (corfu-mode -1)))
+
+(with-eval-after-load 'consult
+  (setq consult-async-min-input 1
+        consult-async-input-debounce 0.01
+        consult-async-input-throttle 0.05))
+
+;; todo; tree sitter
+
+
+(global-set-key (kbd "TAB") #'indent-for-tab-command)
+
+
+(pm/use 'blamer)
+(global-set-key (kbd "C-c g b") #'blamer-mode)
+(pm/use 'diff-hl)
+(global-set-key (kbd "C-c g d") #'diff-hl-mode)
